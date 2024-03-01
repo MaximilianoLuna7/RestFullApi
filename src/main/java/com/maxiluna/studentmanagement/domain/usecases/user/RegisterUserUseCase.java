8e1d854b26d2bbad21e255b0d7dc059723907664
Maximiliano Luna <maxiespn21@gmail.com>
2024-03-01 16:36:55 -0300

    Delete class

--- src/main/java/com/maxiluna/studentmanagement/domain/usecases/user/RegisterUserUseCase.java
+++
@@ -1,7 +0,0 @@
-package com.maxiluna.studentmanagement.domain.usecases.user;
-
-import com.maxiluna.studentmanagement.domain.models.User;
-
-public interface RegisterUserUseCase {
-    User register(User userToRegister);
-}
