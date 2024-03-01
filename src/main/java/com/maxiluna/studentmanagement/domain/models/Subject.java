4208bacbbf97cd4cec5ea716c367d6f255561ecc
Maximiliano Luna <maxiespn21@gmail.com>
2024-03-01 16:38:52 -0300

    add class

---
+++ src/main/java/com/maxiluna/studentmanagement/domain/models/Subject.java
@@ -0,0 +1,29 @@
+package com.maxiluna.studentmanagement.domain.models;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Subject {
+
+    private Long id;
+
+    @NotBlank(message = "Name must not be blank")
+    @Size(min = 2, max = 100, message = "Name must be between 2 and 100 characters")
+    private String name;
+
+    @NotNull(message = "Academic year most not be null")
+    @Min(value = 2000, message = "Academic year must be greater than or equal to 2000")
+    private Integer academicYear;
+
+    private User teacher;
+}
