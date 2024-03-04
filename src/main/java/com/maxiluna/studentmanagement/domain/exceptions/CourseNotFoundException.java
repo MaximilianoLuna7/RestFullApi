e0f01f620a547d5382afb74cdd33751e5211fa73
Maximiliano Luna <maxiespn21@gmail.com>
2024-03-04 15:00:49 -0300

    create new exception

---
+++ src/main/java/com/maxiluna/studentmanagement/domain/exceptions/CourseNotFoundException.java
@@ -0,0 +1,11 @@
+package com.maxiluna.studentmanagement.domain.exceptions;
+
+public class CourseNotFoundException extends RuntimeException{
+    public CourseNotFoundException(String message) {
+        super(message);
+    }
+
+    public CourseNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
