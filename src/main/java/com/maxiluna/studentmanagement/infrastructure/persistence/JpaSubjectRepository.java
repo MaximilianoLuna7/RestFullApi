df7096c5b6bd4cfffd06ca73a29b9ab8a3a8a46e
Maximiliano Luna <maxiespn21@gmail.com>
2024-03-28 12:40:29 -0300

    add new repository methods

--- src/main/java/com/maxiluna/studentmanagement/infrastructure/persistence/JpaSubjectRepository.java
+++ src/main/java/com/maxiluna/studentmanagement/infrastructure/persistence/JpaSubjectRepository.java
@@ -2,6 +2,12 @@ package com.maxiluna.studentmanagement.infrastructure.persistence;
 
 import com.maxiluna.studentmanagement.infrastructure.entities.SubjectJpa;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface JpaSubjectRepository extends JpaRepository<SubjectJpa, Long> {
+    List<SubjectJpa> findByTeacherId(Long teacherId);
+    List<SubjectJpa> findByCourseId(Long courseId);
 }
