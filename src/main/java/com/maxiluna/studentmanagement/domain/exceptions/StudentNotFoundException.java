a30776d49444e4745d54ed350edc24f88732b0d2
Maximiliano Luna <maxiespn21@gmail.com>
2024-04-18 16:09:23 -0300

    create exception

---
+++ src/main/java/com/maxiluna/studentmanagement/domain/exceptions/StudentNotFoundException.java
@@ -0,0 +1,11 @@
+package com.maxiluna.studentmanagement.domain.exceptions;
+
+public class StudentNotFoundException extends RuntimeException {
+    public StudentNotFoundException(String message) {
+        super(message);
+    }
+
+    public StudentNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
