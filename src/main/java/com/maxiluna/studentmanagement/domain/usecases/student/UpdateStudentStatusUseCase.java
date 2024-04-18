e2f362a512cf2bb1ffea23e0e053890e82b589b3
Maximiliano Luna <maxiespn21@gmail.com>
2024-04-18 18:55:39 -0300

    delete use case

--- src/main/java/com/maxiluna/studentmanagement/domain/usecases/student/UpdateStudentStatusUseCase.java
+++
@@ -1,7 +0,0 @@
-package com.maxiluna.studentmanagement.domain.usecases.student;
-
-import com.maxiluna.studentmanagement.domain.models.StudentStatus;
-
-public interface UpdateStudentStatusUseCase {
-    void execute(Long studentId, Long subjectId, StudentStatus updatedStatus);
-}
